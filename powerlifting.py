2b5a7129956889cb8a3c1346ed973d5b948a755c
shea7073 <57239958+shea7073@users.noreply.github.com>
2019-12-09 16:06:12 -0500

    Add files via upload

--- powerlifting.py
+++ powerlifting.py
@@ -4,29 +4,36 @@ from tkinter import *
 
 root = Tk()
 
+
+label1 = Label
+
 def kilo2pound():
     a = e1.get()
     a = float(a)
     a = a*2.2046226218
     print(a)
 
+
 def pound2kilo():
     b = e4.get()
     b = float(b)
     b = b/2.2046226218
-    label = Label(text=b)
 
-button1 = Button(text="calculate kilos to pounds", command=kilo2pound,)
+
+
+
+
+button1 = Button(text="calculate kilos to pounds", command=kilo2pound)
 button2 = Button(text="calculate wilks score")
 button3 = Button(text="calculate 1 rep max")
 button4 = Button(text="calculate pounds to kilos", command=pound2kilo)
-
 
 e1 = Entry(root)
 e2 = Entry(root)
 e3 = Entry(root)
 e4 = Entry(root)
 
+label1 = Label(text = "fuck this shit")
 e1.grid(row=0)
 button1.grid(row=1)
 e2.grid(row=2)
@@ -35,5 +42,6 @@ e3.grid(row=4)
 button3.grid(row=5)
 e4.grid(row=6)
 button4.grid(row=7)
+label1.grid(row=8)
 
 root.mainloop()
